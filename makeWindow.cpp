493f0e371d6a57079a558ed532b9a89122636e3a
KiTaeUK <79833715+KiTaeUK@users.noreply.github.com>
2021-06-11 22:47:17 +0900

    Add files via upload

---
+++ makeWindow.cpp
@@ -0,0 +1,25 @@
+//콘솔 창의 크기와 제목을 지정하는 함수
+void SetGameScreen()
+{
+    system("mode con:cols=100 lines=25");
+    system("title Upgrade_Dino");
+}
+
+//커서의 위치를 x, y로 이동하는 함수
+void GotoXY(int x, int y)
+{
+    COORD Pos;
+    Pos.X = 2 * x;
+    Pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+//키보드의 입력을 받고, 입력된 키의 값을 반환하는 함수
+int GetKeyDown()
+{
+    if (_kbhit() != 0)
+    {
+        return _getch();
+    }
+    return 0;
+}
